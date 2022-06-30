e4f73ca0c393551d389eaf7267872ea1031bbc79
Anderson <anderson.schmidt90@gmail.com>
2022-06-30 08:37:32 -0300

    Terminado Classe Campo

--- src/br/com/cod3r/cm/modelo/Campo.java
+++ src/br/com/cod3r/cm/modelo/Campo.java
@@ -119,4 +119,60 @@ public class Campo {
 		
 	}
 
+	public int getLinha() {
+		return linha;
+	}
+
+	public int getColuna() {
+		return coluna;
+	}
+	
+	boolean ojetivoAcancado() {
+		
+		boolean desvendado = !minado && aberto;
+		boolean protegido = minado && marcado;
+		return desvendado || protegido;
+		
+	}
+	
+	long minasNaVizinhanca() {
+		
+		return vizinhos.stream().filter(v -> v.minado).count();
+		
+	}
+	
+	void reiniciar() {
+		
+		aberto = false;
+		minado = false;
+		marcado = false;
+		
+	}
+	
+	public String toString() {
+		
+		if(marcado) {
+			
+			return "x";
+			
+		}else if(aberto && minado) {
+			
+			return "*";
+			
+		}else if(aberto && minasNaVizinhanca() > 0) {
+			
+			return Long.toString(minasNaVizinhanca());
+			
+		}else if(aberto) {
+			
+			return " ";
+			
+		}else {
+			
+			return "?";
+			
+		}
+		
+	}
+
 }
