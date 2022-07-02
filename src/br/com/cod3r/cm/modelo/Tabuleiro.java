35e861d29e9236cf60b74f0d982dba9396f5e187
Anderson <anderson.schmidt90@gmail.com>
2022-07-02 09:08:49 -0300

    Criado Classe main Aplicação e criado método toString e métodos abrir e alterarMarcação

--- src/br/com/cod3r/cm/modelo/Tabuleiro.java
+++ src/br/com/cod3r/cm/modelo/Tabuleiro.java
@@ -24,6 +24,22 @@ public class Tabuleiro {
 		sortearMinas();
 		
 	}
+	
+	public void abrir(int linha, int coluna) {
+		
+		campos.parallelStream()
+		.filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
+		.findFirst().ifPresent(c -> c.abrir());
+		
+	}
+	
+	public void alternarMarcacao(int linha, int coluna) {
+		
+		campos.parallelStream().
+		filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
+		.findFirst().ifPresent(c -> c.alternarMarcacao());
+		
+	}
 
 	private void gerarCampos() {
 		
@@ -78,6 +94,30 @@ public class Tabuleiro {
 		
 		campos.stream().forEach(c -> c.reiniciar());
 		sortearMinas();
+		
+	}
+	
+	public String toString(){
+		
+		StringBuilder sb = new StringBuilder();
+		
+		int i = 0;
+		for(int l = 0; l < linhas; l++) {
+			
+			for(int c = 0; c < colunas; c++) {
+				
+				sb.append(" ");
+				sb.append(campos.get(i));
+				sb.append(" ");
+				i++;
+				
+			}
+			
+			sb.append("\n");
+			
+		}
+		
+		return sb.toString();
 		
 	}
 	
